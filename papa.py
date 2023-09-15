2844a4418cada1de402471343c89c03fe5eb2156
itzmeproman <atulpandey3030@gmail.com>
2023-09-15 08:20:04 +0530

    Update papa.py

--- papa.py
+++ papa.py
@@ -1,58 +1,64 @@
-from pyrogram import Client, filters
-from pyrogram.handlers import MessageHandler
-from pyrogram.types import Message
-import ffmpeg
+# Importing the required libraries
 import os
-from tqdm import tqdm
+import telebot
+from PIL import Image, ImageDraw, ImageFont
+from telethon import TelegramClient
 
-TOKEN = os.getenv('5995368320:AAHhq6gZ0M-EHSfX6w96XXUUN-Z7oSg7S5w')
-api_id = '20210345'
-api_hash = '11bcb58ae8cfb85168fc1f2f8f4c04c2'
+# Setting the bot token, API ID, API hash and watermark text
+TOKEN = "6591286139:AAFQCCfj7uQ5Lp0beXBmNPjm26VABvCVHJ4"
+API_ID = "20210345"
+API_HASH = "11bcb58ae8cfb85168fc1f2f8f4c04c2"
+WATERMARK = "Anime Compass 🧭"
 
-app = Client("my_account", api_id=api_id, api_hash=api_hash, bot_token=TOKEN)
+# Creating a telebot instance
+bot = telebot.TeleBot(TOKEN)
 
-resolution = '640x480'  # Default resolution
+# Creating a TelegramClient instance
+client = TelegramClient("bot", API_ID, API_HASH)
 
-@app.on_message(filters.command("start"))
-async def start(client: Client, message: Message):
-    await message.reply_text('Hello! Send me a video and I will encode it.')
+# Defining a function to add watermark to an image
+def add_watermark(image):
+  # Opening the image and getting its size
+  im = Image.open(image)
+  width, height = im.size
 
-@app.on_message(filters.command("set_resolution"))
-async def set_resolution(client: Client, message: Message):
-    global resolution
-    resolution = message.text.split()[1]
-    await message.reply_text(f'Set resolution to {resolution}.')
+  # Creating a new image object with the same mode and size as the original image
+  new_im = Image.new(im.mode, (width, height))
 
-@app.on_message(filters.video)
-async def encode_video(client: Client, message: Message):
-    file_path = await client.download_media(
-        message=message,
-        progress=progress,
-        progress_args=("Downloading", message)
-    )
+  # Drawing the watermark text on the new image object
+  draw = ImageDraw.Draw(new_im)
+  font = ImageFont.truetype("arial.ttf", 32) # You can change the font and size here
+  text_width, text_height = draw.textsize(WATERMARK, font)
+  x = width - text_width - 10 # You can change the margin here
+  y = 10 # You can change the margin here
+  draw.text((x, y), WATERMARK, font=font, fill=(255, 255, 255)) # You can change the color here
 
-    process = (
-        ffmpeg
-        .input(file_path)
-        .output('output.mp4', vf=f'scale={resolution}', watermark='Anime Compass 🧭')
-        .run_async(pipe_stdout=True, pipe_stderr=True)
-    )
+  # Pasting the new image object over the original image
+  im.paste(new_im, (0, 0), new_im)
 
-    for line in tqdm(iter(process.stderr.readline, b''), desc="Encoding"):
-        pass
+  # Saving the modified image
+  im.save(image)
 
-    await client.send_video(
-        chat_id=message.chat.id,
-        video='output.mp4',
-        progress=progress,
-        progress_args=("Uploading", message)
-    )
+# Defining a handler for photo messages
+@bot.message_handler(content_types=["photo"])
+def photo_handler(message):
+  # Getting the file ID and file path of the photo message
+  file_id = message.photo[-1].file_id
+  file_path = bot.get_file(file_id).file_path
 
-    os.remove(file_path)
-    os.remove('output.mp4')
+  # Downloading the photo to a local file using Telethon client
+  file_name = file_path.split("/")[-1]
+  client.download_media(file_id, file_name)
 
-def progress(current: int, total: int, prefix: str, message: Message):
-    message.edit_text(f'{prefix}: {current * 100 / total:.1f}%')
+  # Adding watermark to the photo
+  add_watermark(file_name)
 
-app.run()
+  # Sending the modified photo back to the user using Telethon client
+  client.send_file(message.chat.id, file_name)
+
+  # Deleting the local file
+  os.remove(file_name)
+
+# Polling for updates using Telebot instance
+bot.polling()
 
